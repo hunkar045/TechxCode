c62bd7077bd6fcae7bf171f4c53f3d3e20c32ac4
hunkar045 <chawarehunkar@gmail.com>
2026-02-01 00:14:48 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,10 @@
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  role: { type: String, default: "citizen" }
+});
+
+module.exports = mongoose.model("User", UserSchema);
