ac5e7fa5d60fc777f3308f2b2d32e72bf2337007
hunkar045 <chawarehunkar@gmail.com>
2026-01-31 17:54:55 +0530

    Update grievanceRoutes.js

--- backend/routes/grievanceRoutes.js
+++ backend/routes/grievanceRoutes.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const Grievance = require("../models/Grievance");
 
+router.post("/submit", async (req, res) => {
+  const grievance = new Grievance(req.body);
+  await grievance.save();
+  res.json({ message: "Grievance submitted successfully" });
+});
+
+router.get("/", async (req, res) => {
+  const data = await Grievance.find();
+  res.json(data);
+});
+
+module.exports = router;
