6a8d6d0068d448ae3a4d0119884486e2750d1b4f
hunkar045 <chawarehunkar@gmail.com>
2026-01-31 17:30:41 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,8 @@
+const mongoose = require("mongoose");
 
+const connectDB = async () => {
+  await mongoose.connect("mongodb://127.0.0.1:27017/civicflow");
+  console.log("MongoDB Connected");
+};
+
+module.exports = connectDB;
