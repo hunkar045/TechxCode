a4a37a70b4ff59312a00f976862bd18be98a6053
hunkar045 <chawarehunkar@gmail.com>
2026-02-01 00:31:38 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,10 @@
+import ComplaintForm from "./components/ComplaintForm";
 
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>CivicPulse</h1>
+      <ComplaintForm />
+    </div>
+  );
+}
