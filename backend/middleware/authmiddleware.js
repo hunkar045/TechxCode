e18a74d006d9108a28778a34cf5cbc7ac78fe4ce
hunkar045 <chawarehunkar@gmail.com>
2026-02-01 01:03:36 +0530

    Update authmiddleware.js

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -1,1 +1,13 @@
+const jwt = require("jsonwebtoken");
 
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json("Access denied");
+
+  try {
+    jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch {
+    res.status(401).json("Invalid token");
+  }
+};
