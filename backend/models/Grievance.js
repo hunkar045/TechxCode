acb65db9fa787d994955dfe87fa041a32243accf
hunkar045 <chawarehunkar@gmail.com>
2026-01-31 17:51:08 +0530

    Update Grievance.js

--- backend/models/Grievance.js
+++ backend/models/Grievance.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
 
+const grievanceSchema = new mongoose.Schema({
+  citizenName: String,
+  category: String,
+  description: String,
+  department: String,
+  status: { type: String, default: "Pending" },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Grievance", grievanceSchema);
