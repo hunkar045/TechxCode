c0877227ea799a67f7ff776649a4627b01928b42
hunkar045 <chawarehunkar@gmail.com>
2026-02-01 00:23:42 +0530

    Update complaintController.js

--- backend/controllers/complaintController.js
+++ backend/controllers/complaintController.js
@@ -1,1 +1,20 @@
+const Complaint = require("../models/Complaint");
+const getPriority = require("../utils/priorityEngine");
 
+exports.createComplaint = async (req, res) => {
+  const priority = getPriority(req.body.category);
+  const slaHours = priority === "High" ? 12 : 48;
+
+  const complaint = await Complaint.create({
+    ...req.body,
+    priority,
+    slaDeadline: new Date(Date.now() + slaHours * 3600000)
+  });
+
+  res.json(complaint);
+};
+
+exports.getComplaints = async (req, res) => {
+  const data = await Complaint.find();
+  res.json(data);
+};
