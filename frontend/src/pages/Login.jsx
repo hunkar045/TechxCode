e6bc9101bb8f1f3f6a678cf05f27ff592d0e3ac9
hunkar045 <chawarehunkar@gmail.com>
2026-02-01 01:06:12 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,1 +1,12 @@
+import "../styles/main.css";
 
+export default function Login() {
+  return (
+    <div className="card">
+      <h2>Citizen Login</h2>
+      <input placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button>Login</button>
+    </div>
+  );
+}
