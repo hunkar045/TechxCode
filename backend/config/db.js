8b396d1528dfaade86ae57faecfedde7a2c2ab30
hunkar045 <chawarehunkar@gmail.com>
2026-01-31 23:40:40 +0530

    Delete backend/config directory

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/civicflow");
-  console.log("MongoDB Connected");
-};
-
-module.exports = connectDB;
