94127e09c86759a8ccc23732cde03647bc4bbd25
hunkar045 <chawarehunkar@gmail.com>
2026-01-31 17:56:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const grievanceRoutes = require("./routes/grievanceRoutes");
+const cors = require("cors");
 
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/grievances", grievanceRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
